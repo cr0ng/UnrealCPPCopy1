8edfbd4bfff662746d1d89e76f4f5a2d9ffd29e4
yujin <yj98k@naver.com>
2025-11-10 12:01:50 +0900

    캐릭터 입력처리 설정 및 에셋 추가

---
+++ Source/KI_UnrealCPP/Private/Player/ActionPlayerController.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/ActionPlayerController.h"
+#include "EnhancedInputSubSystems.h"
+#include "inputMappingContext.h"
+
+void AActionPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = 
+		ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()); // Subsystem 가져오기 시도
+
+	if (Subsystem)	// Subsystm이 null이 아니면
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, GameInputPriority);
+	}
+
+}
